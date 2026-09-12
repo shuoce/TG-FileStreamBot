0f0a74a8da2e86355f5399b68eb95e7e1a890ed7
shuoce <2765414002@qq.com>
2026-09-12 14:13:23 +0800

    update start message

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -49,7 +49,7 @@ func start(ctx *ext.Context, u *ext.Update) error {
 
 	_, err := ctx.Reply(
 		u,
-		ext.ReplyTextString("👋 欢迎使用 Shuoce File Stream
+		ext.ReplyTextString(`👋 欢迎使用 Shuoce File Stream
 
 📁 发送文件、视频、图片或音频
 🔗 自动生成可访问的直链
@@ -59,9 +59,8 @@ func start(ctx *ext.Context, u *ext.Update) error {
 请勿使用本机器人存储、传播任何违法违规内容。
 请合理使用本服务，由此产生的一切责任由使用者自行承担。
 
-感谢你的使用 ❤️"),
+感谢你的使用 ❤️`),
 		nil,
 	)
 
 	return err
-}
